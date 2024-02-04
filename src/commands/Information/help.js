d60d1b53a402f1b850ef8ab9f0879adf488d2a8d
TheExtremezCoder <156786126+TheExtremezCoder@users.noreply.github.com>
2024-02-04 14:19:40 +0530

    Add files via upload

---
+++ src/commands/Information/help.js
@@ -0,0 +1,93 @@
+const { MessageEmbed, MessageActionRow, MessageButton, MessageSelectMenu } = require("discord.js");
+
+module.exports = {
+    name: "help",
+    category: "Information",
+    aliases: [ "h" ],
+    description: "Get Help Menu",
+    args: false,
+    usage: "",
+    permission: [],
+    owner: false,
+   execute: async (message, args, client, prefix) => {
+
+      
+        const lawde = new MessageActionRow()
+	.addComponents(
+       new MessageButton()
+ .setLabel("Invite")
+ .setStyle("LINK")
+  .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`), 
+       new MessageButton()
+   .setLabel("Support")
+ .setStyle("LINK")
+    .setURL("https://discord.gg/r4GnJYNMgE"),
+    new MessageButton()
+  .setLabel("Vote")   
+ .setStyle("LINK")  
+   .setURL(`https://top.gg/bot/${client.user.id}/vote`), 
+	        ) 
+
+
+
+let helpmenu = new MessageEmbed()
+
+   
+
+        .setAuthor({name : `${client.user.username} Help Menu;`,iconURL : client.user.displayAvatarURL()})
+  .setThumbnail("https://media.discordapp.net/attachments/1162795987014787162/1200740969843269702/1199357089395527723.png?ex=65c7482b&is=65b4d32b&hm=8a8993a04cbbeece7bccbd18fab4fb5b89eb6bae69bd16b29b408bfdb080d0ed&=&format=webp&quality=lossless&width=160&height=61")
+  
+.setDescription(`➡ Prefix for this server is: **${prefix}**\n➡ Total Commands: ${client.commands.size}\n➡ [Get ${client.user.username}](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot) | [Support](https://discord.gg/r4GnJYNMgE) | [Vote](https://top.gg/bot/${client.user.id}/vote)\n\n🛜 **__All Modules:__**\n\nℹ️ : Info\n🎧 : Music\n🎤 : Filters\n🛠️ : Utility\n⚙️ : Config\n📖 : All Commands`)
+
+
+
+      //  .setFooter(ee.footertext, ee.footericon)
+        .setFooter({text : `Thanks For Using This Bot!!` , iconURL : `https://media.discordapp.net/attachments/1162795987014787162/1200740969843269702/1199357089395527723.png?ex=65c7482b&is=65b4d32b&hm=8a8993a04cbbeece7bccbd18fab4fb5b89eb6bae69bd16b29b408bfdb080d0ed&=&format=webp&quality=lossless&width=160&height=61`}).setThumbnail(client.user.displayAvatarURL({dynamic : true}))
+     .setColor("#ff0000")
+     .setImage("https://media.discordapp.net/attachments/1162795987014787162/1198544044050092092/standard.gif?ex=65c8849f&is=65b60f9f&hm=7bda3e2b1b67d0d0aa48c5ae793d5168e22b6aa42556fe140d7b487ee8557a73&=&width=850&height=300")
+
+     
+        const row = new MessageActionRow()
+        .addComponents(
+            new MessageSelectMenu()
+                .setCustomId('help')
+                .setPlaceholder('Select Module To Get Help For That Module')
+                .addOptions([
+                {
+                    label: 'Info',
+                    description: 'Show You Information Commands',
+                    value: 'first',
+                  emoji: 'ℹ️'
+                },
+                {
+                    label: 'Music',
+                    description: 'Show You Music Commands',
+                    value: 'second',
+                    emoji: '🎧'
+                },
+{
+                    label: 'Filters',
+                    description: 'Show You Music Filter Commands',
+                    value: 'fourth',
+       emoji: '🎤'                },   
+                  {
+                    label: 'Utility',
+                    description: 'Show You Utility Commands',
+                    value: 'sixth',
+       emoji: '🛠️'                },         
+                {
+                    label: 'Config',
+                    description: 'Show You Configuration Commands',
+                    value: 'fifth',
+                   emoji: '⚙️'                },
+                {
+                    label: 'All Commands',
+                    description: 'Show You All Commands',
+                    value: 'third',
+       emoji: '📖'                }           
+            ])
+        )
+        if (!args[0]) return message.reply({embeds: [helpmenu], components: [row,lawde]});
+     //message.reply({ embeds: [helpmenu], components: [row] })
+   }
+}
---
+++ src/config.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+module.exports = {
+  token: process.env.TOKEN || "MTE5NzU4NTg4MjgwMzM0MzM5MQ.GKL8aV.B7PhGIbX6nLieav0S5GQC01rGHlicMk7k3cdtA",  // your bot token
+  prefix: process.env.PREFIX || ".", // bot prefix
+  ownerID: process.env.OWNERID || ["1055517646570922074"], //your discord id
+  mongourl: process.env.MONGO_URI || "mongodb+srv://Rayzmusic:music@cluster0.rhzwp8e.mongodb.net/?retryWrites=true&w=majority", // MongoDb URL
+  topggapi: "",
+  embedColor: process.env.COlOR || "#2f3136", // embed colour
+  logs: process.env.LOGS || "1092913996467818567", // channel id for guild create and delete logs
+  langs: process.env.LANGS || "en",
+
+
+  nodes: [
+    {
+      host: process.env.NODE_HOST || "lava.link",
+      identifer: process.env.NODE_ID || "Main",
+      port: parseInt(process.env.NODE_PORT || "80"),
+      password: process.env.NODE_PASSWORD || "LAVA",
+      secure: parseBoolean(process.env.NODE_SECURE || "false"),
+
+    }
+  ],
+
+
+}
+
+function parseBoolean(value) {
+  if (typeof (value) === 'string') {
+    value = value.trim().toLowerCase();
+  }
+  switch (value) {
+    case true:
+    case "true":
+      return true;
+    default:
+      return false;
+  }
+}
